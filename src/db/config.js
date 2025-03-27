494c4081a65bca517a775a498d184b35b4d22f14
aliaahmedraza <aliaahmedraza@gmail.com>
2025-03-27 16:42:18 +0500

    lastUpdation

--- src/db/config.js
+++ src/db/config.js
@@ -1,18 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const dbURI = process.env.MONGO_URL || process.env
 
-const dbConfig = async () => {
-  try {
-    await mongoose.connect("mongodb+srv://aliahmedrza:ritadanto@blogapp.iz2b4.mongodb.net/?retryWrites=true&w=majority&appName=blogApp", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 30000, // Increase timeout to 30s
-      socketTimeoutMS: 45000, // Increase socket timeout
-    });
-    console.log("✅ MongoDB connected");
-  } catch (error) {
-    console.error("❌ MongoDB connection error:", error);
-    process.exit(1);
-  }
-};
+const dbConfig = mongoose
+  .connect(dbURI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 export default dbConfig;
