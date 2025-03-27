e38f95db17466c3211c2e04033f737b46dc0ff66
aliaahmedraza <aliaahmedraza@gmail.com>
2025-03-27 16:30:21 +0500

    lastUpdation

--- src/db/config.js
+++ src/db/config.js
@@ -1,21 +1,18 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
 dotenv.config();
+const dbURI = process.env.MONGO_URL || "mongodb+srv://aliahmedrza:ritadanto@blogapp.iz2b4.mongodb.net/?retryWrites=true&w=majority&appName=blogApp";
 
-const dbURI = process.env.MONGO_URL || "mongodb+srv://aliahmedrza:ritadanto@blogapp.iz2b4.mongodb.net/?retryWrites=true&w=majority&appName=blogApp"; // Use a default local DB if env var is missing
-
-const connectDB = async () => {
+const dbConfig = async () => {
   try {
-    await mongoose.connect(dbURI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("✅ MongoDB Connected Successfully!");
+    console.log("MongoDB Connected...");
   } catch (error) {
-    console.error("❌ MongoDB Connection Error:", error.message);
-    process.exit(1); // Exit process on failure
+    console.error("MongoDB Connection Error:", error);
+    process.exit(1);
   }
 };
-
 export default dbConfig;
